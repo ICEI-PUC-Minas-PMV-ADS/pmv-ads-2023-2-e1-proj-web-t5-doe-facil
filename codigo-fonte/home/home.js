769f5235afa951d9ecdce7ad94289d449737a2ab
Carlos Roberto <crpu4a@gmail.com>
2023-10-31 18:37:18 -0300

    feat: adicionando menu nas páginas

--- codigo-fonte/home/home.js
+++ codigo-fonte/home/home.js
@@ -1,4 +1,6 @@
-var elem = document.getElementById('btn-serDoador');
-    elem.addEventListener('click',
-        function () { location.href = "http://codigo-fonte/login/"; 
-    };
+$g_makeMenu()
+
+var elem = document.getElementById('btn-serDoador')
+elem.addEventListener('click', function () {
+    location.href = 'http://codigo-fonte/login/'
+})
