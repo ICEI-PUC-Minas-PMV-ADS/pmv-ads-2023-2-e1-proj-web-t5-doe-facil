e87ed085dfb025456de8db8d4d06dfcaa66c5d9b
jessica-sreis <144929767+jessica-sreis@users.noreply.github.com>
2023-10-30 22:47:24 -0300

    texto incluido no Js

---
+++ codigo-fonte/about/about.js
@@ -0,0 +1,20 @@
+$g_makeMenu()
+
+const textoIntroducao = document.querySelector("#texto-introducao");
+
+textoIntroducao.textContent = `Idealizada no ano de 2023, a Doe Fácil! é uma aplicação que tem o objetivo de
+ser um facilitador para pessoas que têm o desejo de doar algo e instituições que precisam receber doações.
+Viemos com o intuito de auxiliar na conexão entre doadores e instituições sem fins lucrativos, na intenção de
+ajudar pessoas que queiram e tenham algo a doar, porém, não sabem quais lugares estão necessitando do bem a
+ser doado, bem como ajudar as instituições a alcançar mais doadores, apresentando a todos, a suafinalidade,
+suas necessidades e seu impacto na sociedade.`
+
+const textoSecundario = document.querySelector("#text-secundario");
+
+textoSecundario.textContent = `Ajudamos o maior número de Instituições sem fins lucrativos a receberem mais
+doações, auxiliar na visibilidade das mesmas e filtrar da melhor maneira possīvel suas necessidades, como
+principais doações necessárias no momento ou regularmente.Qualquer pessoa pode ser um doador, desta maneira,
+    nossa plataforma auxilia na busca pela Instituição que melhor atende o doador em relação ao item a ser doado
+e à localização.Aumentando também o conhecimento de outras Instituições e seus objetivos.`
+
+
