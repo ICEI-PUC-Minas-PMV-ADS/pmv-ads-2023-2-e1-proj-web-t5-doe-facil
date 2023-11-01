34c2dc2e90d505e255207101c8cadb8cd2c77e63
Guilherme Radicchi <guilhermeradicchi@hotmail.com>
2023-11-01 12:58:52 -0300

    Melhorias CSS, Carrossel JS

--- codigo-fonte/home/home.js
+++ codigo-fonte/home/home.js
@@ -1,7 +1,20 @@
-let count = 1;
-$g_makeMenu()
-
 var elem = document.getElementById('btn-serDoador')
 elem.addEventListener('click', function () {
     location.href = 'http://codigo-fonte/login/'
 })
+
+let count = 1;
+document.getElementById("radio1").checked = true;
+
+setInterval( function(){
+    nextImage();
+}, 2000)
+
+function nextImage(){
+    count++
+    if(count>3){
+    count = 1;
+    }
+    
+    document.getElementById("radio1"+count).checked = true;
+}
