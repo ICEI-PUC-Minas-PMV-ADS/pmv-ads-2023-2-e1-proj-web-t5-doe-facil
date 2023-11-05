3e3a864635fb80c1f828a2fdaee1a17aa5ec2e9e
Bárbara Figueiredo <barbaraflassis@gmail.com>
2023-11-05 20:47:54 -0300

    Mudança da importação do script
    Mudança da importação do script do footer para o head.

--- codigo-fonte/dashboard/donations/donations.js
+++ codigo-fonte/dashboard/donations/donations.js
@@ -1,2 +1,9 @@
 $g_makeMenu()
 $g_makeLateralMenu('donations')
+
+const editBtList = document.querySelectorAll(".editDoacao") 
+editBtList.forEach(function(editBtn){
+editBtn.onclick = function (e){
+    console.log('fuiClicado', e)
+}
+})
