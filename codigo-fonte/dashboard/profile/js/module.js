7ae66cfcf1c5d1b3273bdef759144cf82dddb1fb
Carlos Roberto <crpu4a@gmail.com>
2023-11-05 19:06:11 -0300

    refactor: refatoração de profile

--- codigo-fonte/dashboard/profile/js/module.js
+++ codigo-fonte/dashboard/profile/js/module.js
@@ -1,11 +1,11 @@
 'use strict'
 
 import {
-    createCheckBoxElements,
     submitAboutForm,
     submitChangePasswordForm,
     submitForm,
 } from './form.js'
+import { mountForms } from './lifecycles.js'
 
 const donatorForm = document.querySelector('#donator-form')
 const institutionForm = document.querySelector('#institution-form')
@@ -31,25 +31,6 @@ passwordForm.addEventListener('submit', (e) => {
     e.preventDefault()
     submitChangePasswordForm(passwordForm)
 })
-
-const mountForms = () => {
-    const user = $g_getSessionUser()
-    if (user.type === 'donator') {
-        $g_injectInputForm(user, donatorForm, 'donator_')
-        institutionForm.style.display = 'none'
-        institutionAboutForm.style.display = 'none'
-        document.querySelector('.extra__block').style.display = 'none'
-    } else {
-        $g_injectInputForm(user, institutionForm, 'institution_')
-        $g_injectInputForm(user, institutionAboutForm, 'institution_')
-
-        createCheckBoxElements(
-            $g_getDonationTypesInput(user.types),
-            document.querySelector('#institution-types')
-        )
-        donatorForm.style.display = 'none'
-    }
-}
 
 mountForms()
 
