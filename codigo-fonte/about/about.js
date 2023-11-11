bde578fd774daa8d394c36cd3e38045b367cefb8
jessica-sreis <144929767+jessica-sreis@users.noreply.github.com>
2023-11-10 21:16:24 -0300

    Adicionado get para obter informação  pagína sobre.

--- codigo-fonte/about/about.js
+++ codigo-fonte/about/about.js
@@ -1,18 +1,25 @@
-const textoIntroducao = document.querySelector("#texto-introducao");
+import { $g_getAboutInfo } from "../public/js/about.js";
 
-textoIntroducao.textContent = `Idealizada no ano de 2023, a Doe Fácil é uma aplicação que tem o objetivo de
-ser um facilitador para pessoas que têm o desejo de doar algo e instituições que precisam receber doações.
-Viemos com o intuito de auxiliar na conexão entre doadores e instituições sem fins lucrativos, na intenção de
-ajudar pessoas que queiram e tenham algo a doar, porém, não sabem quais lugares estão necessitando do bem a
-ser doado, bem como ajudar as instituições a alcançar mais doadores, apresentando a todos, a sua finalidade,
-suas necessidades e seu impacto na sociedade.`
+const pageInfo = $g_getAboutInfo()
+const pageInfoDiv = document.querySelector("#page-info");
 
-const textoSecundario = document.querySelector("#text-secundario");
+const descriptionText = document.createElement('p')
+descriptionText.textContent = pageInfo.description
+descriptionText.classList = 'p--text'
 
-textoSecundario.textContent = `Ajudamos o maior número de Instituições sem fins lucrativos a receberem mais
-doações, auxiliar na visibilidade das mesmas e filtrar da melhor maneira possīvel suas necessidades, como
-principais doações necessárias no momento ou regularmente. Qualquer pessoa pode ser um doador, desta maneira,
-    nossa plataforma auxilia na busca pela Instituição que melhor atende o doador em relação ao item a ser doado
-e à localização. Aumentando também o conhecimento de outras Instituições e seus objetivos.`
+
+const subTitle = document.createElement('h2')
+subTitle.textContent = pageInfo.sub_title
+
+const roleText = document.createElement('p')
+roleText.textContent = pageInfo.role
+roleText.classList ='p--text'
+
+pageInfoDiv.appendChild(descriptionText)
+pageInfoDiv.appendChild(subTitle)
+pageInfoDiv.appendChild(roleText)
+
+const pageTitle = document.querySelector("#page-title")
+pageTitle.textContent = pageInfo.title
 
 
