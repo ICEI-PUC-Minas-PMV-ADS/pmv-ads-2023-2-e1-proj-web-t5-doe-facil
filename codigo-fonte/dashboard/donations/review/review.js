53205cd149b3dc065b7d657aea742dcaa09cb78d
jessica-sreis <144929767+jessica-sreis@users.noreply.github.com>
2023-11-17 09:15:29 -0300

    apresentando as informações das doações.

--- codigo-fonte/dashboard/donations/review/review.js
+++ codigo-fonte/dashboard/donations/review/review.js
@@ -19,5 +19,54 @@ function makeDonationTableLines() {
         corpoTabela.appendChild(lineTable)
     }
 }
+function makeDonationInformation() {
+    const donationInfo = $g_getDonationById(id)
+    const detailsDonator = document.querySelector('#details-donator')
+    detailsDonator.innerHTML = `
+
+    <div class="d-flex">
+        <div class="p-2 flex-fill">
+            <p class="inf-donations">DOADOR</p>
+            <p>${donationInfo.name}</p>
+        </div>
+        <div class="p-2 flex-fill">
+            <p class="inf-donations">Do dia</p>
+            <p>${donationInfo.collection_date}</p>
+        </div>
+        <div class="p-2 flex-fill">
+            <p class="inf-donations"> Até o dia</p>
+            <p> ${donationInfo.finish_collection_date}</p>
+            
+        </div>
+        
+    </div>
+    <div class="d-flex">
+        <div class="p-2 flex-fill">
+            <p class="inf-donations">Endereço de Coleta</p>
+            <p>${donationInfo.address}</p>
+        </div>
+        <div class="p-2 flex-fill">
+            <p class="inf-donations">Bairro</p>
+            <p>${donationInfo.neighborhood}</p>
+        </div>
+        <div class="p-2 flex-fill">
+            <p class="inf-donations"> CEP</p>
+            <p> ${donationInfo.cep}</p>
+            
+        </div>
+`
+console.log(donationInfo)
+    for (const donation of donationInfo.donations) {
+        const lineTable = document.createElement('tr')
+        lineTable.innerHTML = `
+            <td>${donation.type}</td>
+            <td>${donation.amount}</td>
+            <td>${donation.description}</td>
+        `
+
+        corpoTabela.appendChild(lineTable)
+    }
+}
 
 makeDonationTableLines()
+makeDonationInformation()
