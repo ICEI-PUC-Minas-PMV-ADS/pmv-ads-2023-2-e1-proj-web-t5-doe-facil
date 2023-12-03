dbbd3ab5f4881213b4bca6d8f9dac5ed56235cb6
Carlos Roberto <crpu4a@gmail.com>
2023-12-03 12:24:12 -0300

    refatoração de mount do formulário de criação de doações para utilizar objeto

--- codigo-fonte/dashboard/donations/create/js/lifecycles.js
+++ codigo-fonte/dashboard/donations/create/js/lifecycles.js
@@ -1,44 +1,83 @@
-import { injectInstitutionsOptions, resetDonationFields } from './form.js'
-import { injectFormDraft } from './draft.js'
-import { $g_getDonationById } from '../../../../public/js/donation.js'
+import { injectInstitutionsOptions, resetDonationFields } from "./form.js";
+import { injectFormDraft } from "./draft.js";
+import { $g_getDonationById } from "../../../../public/js/donation.js";
 
-export const mountForm = () => {
-    const donationElementField = document
-        .querySelectorAll('#donation-form .donation_fields')[0]
-        .cloneNode(true)
+export class DonationForm {
+    elementForm;
+    donationElementField;
+    #pageParams;
+    #donation = null;
 
-    injectInstitutionsOptions()
-    resetDonationFields(donationElementField)
+    constructor() {
+        this.elementForm = this.#getFormElement();
+        this.donationElementField = this.#getDonationElementField();
+        this.#pageParams = this.#getPageParams();
 
-    const params = new URLSearchParams(window.location.search)
-    const id = params.get('id')
-
-    if (id) {
-        const donation = $g_getDonationById(id)
-        if (!donation) return
-
-        const form = document.querySelector('#donation-form')
-        const newInputs = document.createElement('div')
-
-        newInputs.innerHTML = `
-            <input type="hidden" class="form_item" id="id" value="${donation.id}" />
-            <input type="hidden" class="form_item" id="status" value="${donation.status}" />
-            <input type="hidden" class="form_item" id="donator" value="${donation.donator}" />
-            <input type="hidden" class="form_item" id="edited" value="1" />
-        `
-
-        form.appendChild(newInputs)
-
-        const draftCol = form.querySelector('#draft-col')
-        draftCol.remove()
-
-        const submitButton = form.querySelector('#submit')
-        submitButton.textContent = 'Atualizar Doação'
-
-        injectFormDraft(donationElementField, donation)
-    } else {
-        injectFormDraft(donationElementField)
+        if (this.#pageParams.id) {
+            this.#donation = this.#getDonation();
+        }
     }
 
-    return { donationElementField }
+    mount() {
+        injectInstitutionsOptions();
+        resetDonationFields(this.donationElementField);
+
+        if (!this.#donation) injectFormDraft(this.donationElementField);
+
+        this.#addHiddenMetadataInputs();
+
+        const draftCol = this.elementForm.querySelector("#draft-col");
+        draftCol.remove();
+
+        const submitButton = this.elementForm.querySelector("#submit");
+        submitButton.textContent = "Atualizar Doação";
+
+        injectFormDraft(this.donationElementField, this.#donation);
+    }
+
+    #getPageParams() {
+        const params = new URLSearchParams(window.location.search);
+        const id = params.get("id");
+        return { id };
+    }
+
+    #getDonation() {
+        const donation = $g_getDonationById(this.#pageParams.id);
+        return donation ? donation : null;
+    }
+
+    #getFormElement() {
+        return document.querySelector("#donation-form");
+    }
+
+    #getDonationElementField() {
+        return document
+            .querySelectorAll("#donation-form .donation_fields")[0]
+            .cloneNode(true);
+    }
+
+    #addHiddenMetadataInputs() {
+        if (!this.elementForm || !this.#donation) return;
+
+        this.elementForm.appendChild(
+            this.#createHiddenMetadataElements(
+                this.#donation.id,
+                this.#donation.status,
+                this.#donation.donator
+            )
+        );
+    }
+
+    #createHiddenMetadataElements(id, status, donator) {
+        const elements = document.createElement("div");
+
+        elements.innerHTML = `
+            <input type="hidden" class="form_item" id="id" value="${id}" />
+            <input type="hidden" class="form_item" id="status" value="${status}" />
+            <input type="hidden" class="form_item" id="donator" value="${donator}" />
+            <input type="hidden" class="form_item" id="edited" value="1" />
+        `;
+
+        return elements;
+    }
 }
--- codigo-fonte/dashboard/donations/create/js/module.js
+++ codigo-fonte/dashboard/donations/create/js/module.js
@@ -1,32 +1,34 @@
-'use strict'
+"use strict";
 
-import { $g_makeLateralMenu } from '../../../../public/js/components.js'
-import { $g_saveDonationDraft } from '../../../../public/js/donation.js'
-import { $g_checkDonatorPermissions } from '../../../../public/js/permissions.js'
+import { $g_makeLateralMenu } from "../../../../public/js/components.js";
+import { $g_saveDonationDraft } from "../../../../public/js/donation.js";
+import { $g_checkDonatorPermissions } from "../../../../public/js/permissions.js";
 
-import { addDonationFields, getFormInputs, submitForm } from './form.js'
-import { mountForm } from './lifecycles.js'
+import { addDonationFields, getFormInputs, submitForm } from "./form.js";
+import { DonationForm } from "./lifecycles.js";
 
-$g_checkDonatorPermissions('dashboard/donations/create')
-$g_makeLateralMenu('create-donation')
+$g_checkDonatorPermissions("dashboard/donations/create");
+$g_makeLateralMenu("create-donation");
 
-const form = document.querySelector('#donation-form')
-const { donationElementField } = mountForm()
+const donationForm = new DonationForm();
+donationForm.mount();
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault()
-    submitForm(getFormInputs())
-})
+donationForm.elementForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    submitForm(getFormInputs());
+});
 
-const draftButton = form.querySelector('#draft')
+donationForm.elementForm
+    .querySelector("#add-donation-item")
+    .addEventListener("click", (e) => {
+        e.preventDefault();
+        addDonationFields(donationForm.donationElementField);
+    });
+
+const draftButton = donationForm.elementForm.querySelector("#draft");
 if (draftButton) {
-    draftButton.addEventListener('click', (e) => {
-        e.preventDefault()
-        $g_saveDonationDraft(getFormInputs())
-    })
+    draftButton.addEventListener("click", (e) => {
+        e.preventDefault();
+        $g_saveDonationDraft(getFormInputs());
+    });
 }
-
-form.querySelector('#add-donation-item').addEventListener('click', (e) => {
-    e.preventDefault()
-    addDonationFields(donationElementField)
-})
